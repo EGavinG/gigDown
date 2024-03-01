7ea205c404f484e37e0dd2c0dc45348d90c04978
Gavin Garcia <edwardgavingarcia@yahoo.com>
2024-03-01 02:19:52 -0600

    Create a delete fetch that connects to checklist to delete a track off the checklist

--- src/components/ChecklistContent/ChecklistContent.js
+++ src/components/ChecklistContent/ChecklistContent.js
@@ -6,12 +6,6 @@ import './ChecklistContent.css'
 
 function ChecklistContent() {
 
-  const checklistData = [
-    { id: 1, title: "Track 1", artist: 'Artist', downloaded: false },
-    { id: 2, title: "Track 2", artist: 'Artist', downloaded: true },
-    { id: 3, title: "Track 3",  artist: 'Artist',downloaded: false }
-  ];
-
   const getChecklist = () => {
     fetch('http://localhost:3001/checklist')
     .then(response => response.json())
@@ -27,12 +21,49 @@ function ChecklistContent() {
 
   const handleFormSubmit = (trackData) => {
     setChecklist(checklistData => [...checklistData, trackData]);
+    
+    fetch('http://localhost:3001/checklist', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(trackData),
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to add track to checklist');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log('Track added to checklist:', data);
+    })
+    .catch(error => console.error('Error adding track to checklist:', error));
+  };
+
+  const handleDelete = (id) => {
+    deleteTrackInput(id);
+    setChecklist(checklist.filter(track => track.id !== id));
+  };
+
+
+  const deleteTrackInput = (id) => {
+    fetch(`http://localhost:3001/checklist/${id}`, {
+      method: 'DELETE',
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to delete track from checklist');
+      }
+      console.log('Track deleted from checklist');
+    })
+    .catch(error => console.error('Error deleting track from checklist:', error));
   };
 
   return (
     <main className='ChecklistData'>
       <Form onSubmit={handleFormSubmit}/>
-      <Checklist checklist={checklist}/>
+      <Checklist checklist={checklist} onDelete={handleDelete}/>
     </main>
   );
 }
